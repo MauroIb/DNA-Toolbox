fda4af40dc6c0365e75f65c16578f856f2ed2dd4
MauroIb <mauro0094@gmail.com>
2020-10-09 19:47:13 -0300

    tes

--- DNAToolbox/Modules/sequencehandler.py
+++ DNAToolbox/Modules/sequencehandler.py
@@ -4,18 +4,20 @@ from Bio import SeqIO
 import tkinter as tk
 import interface
 
-
+#frame con interfaz
 def screen(frame):
     tk.Label(frame, text='A').pack()
     tk.Button(frame, text='Back to Main',
               command=lambda: interface.interface.goto('main', frame)).pack()
 
-
+# returns seq record obj
 def seq_from_file(f_string, f_option):
     records = list(SeqIO.parse(f_string, f_option))
+    main = SeqIO.read(f_string, f_option)
+    #si quiero la secuencia hago secuencia = main.seq
     return records
 
-
+#returns seq obj
 def seq_from_string(p_string):
     mainSeq = Seq(p_string)
     return mainSeq
