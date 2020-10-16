eb81c120ba5176beb9dc4de5488322b8db38cd23
Mauro <mauro0094@gmail.com>
2020-10-15 22:51:48 -0300

    cambie toda la interfaz

--- DNAToolbox/Modules/interface.py
+++ DNAToolbox/Modules/interface.py
@@ -1,35 +1,45 @@
 import tkinter as tk
-import sequencehandler
+from sequencehandler import *
 
-class Interface():
-    def __init__(self, master):
-        self.master = master
-        self.master.geometry('500x500')
-        frame = tk.Frame(self.master)
-        frame.pack()
-        self.main(frame)
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
 
-    def begin(self):
-        root = tk.Tk()
-        window = Interface(root)
-        root.mainloop()
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-    def goto(self, destination, frame):
-        frame.destroy()
-        frame = tk.Frame(self.master)
-        frame.pack()
-        goto = {
-            'main': self.main,
-            'seqHandler': sequencehandler.screen,
-        }
-        goto[destination](frame)
+        self.frames = {}
+        #Container. The one on top is visible
+        for F in (StartPage, SequenceHandlerScreen):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame("StartPage")
 
-    def main(self, frame):
-        tk.Label(frame, text='Main').pack()
-        tk.Button(frame, text='Goto A', command=lambda: self.goto('seqHandler', frame)).pack()
+    def show_frame(self, page_name):
+            '''Show a frame for the given page name'''
+            frame = self.frames[page_name]
+            frame.tkraise()
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Pagina Principal")
+        label.pack(side="top", fill="x", pady=10)
+        button1 = tk.Button(self, text="Open Sequence Handler",
+                            command=lambda: controller.show_frame("SequenceHandlerScreen"))
+        button1.pack()
 
 
 if __name__ == '__main__':
-        root = tk.Tk()
-        window = Interface(root)
-        root.mainloop()
+    app = App()
+    app.mainloop()
